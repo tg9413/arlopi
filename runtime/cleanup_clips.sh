51a9c6a0617ce1935a17130d64cf28c9ada7585e
Hitaru Terosa <gangtian9413@gmail.com>
2023-01-07 14:30:45 -0800

    Add auto truncation, default 14 days

---
+++ runtime/cleanup_clips.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+CLIP_STORE=/share/arlo
+#RETENTION_DURATION=${1:-14}
+RETENTION_DURATION=14
+
+find "$CLIP_STORE" -maxdepth 5 -type f -mtime +"$RETENTION_DURATION" -print | xargs -r rm
--- setup/setup_cronjob.sh
+++ setup/setup_cronjob.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 init_mass_storage="@reboot sudo sh /home/pi/arlopi/runtime/enable_mass_storage.sh > /home/pi/log/enable_mass_storage_log.txt 2>&1"
 sync_clip_interval="*/1 * * * * sudo /bin/bash /home/pi/arlopi/runtime/sync_clips.sh > /home/pi/log/sync_log.txt 2>&1"
+cleanup_clips_interval="0 0 * * * sudo /bin/bash /home/pi/arlopi/runtime/cleanup_clips.sh > /home/pi/log/cleanup_log.txt 2>&1"
 
 ( crontab -l | cat;  echo "$init_mass_storage" ) | crontab -
 ( crontab -l | cat;  echo "$sync_clip_interval" ) | crontab -
+( crontab -l | cat;  echo "$cleanup_clip_interval" ) | crontab -
 
 crontab -l
 echo "Cron job updated"
